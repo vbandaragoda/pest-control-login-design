183201c1a926bcc479c46806aee0e9d09252bf8d
Vimukthi Bandaragoda <vimukthi.nelanga@gmail.com>
2023-11-19 22:52:38 +0530

    image & respoonsive updates

--- app/page.js
+++ app/page.js
@@ -1,28 +1,22 @@
 "use client";
 import Image from "next/image";
-import vector from "../public/images/vector.svg";
-import pest from "../public/images/pest.svg";
 import peco from "../public/images/peco.svg";
 import google from "../public/images/google.svg";
 import apple from "../public/images/apple.svg";
 import fb from "../public/images/fb.svg";
 import company from "../public/images/company.svg";
+import pestVector from "../public/images/pestVector.svg";
 
 export default function Home() {
   return (
     <main className="content-center font-['Inter']">
-      <div className="grid md:grid-cols-2">
-        <div className="img-side md:col-span-1 flex mx-20 my-16 bg-rose-100 justify-center items-center rounded-2xl">
-          <div className="px-20 py-12 ">
-            <div className="flex justify-end">
-              <Image src={pest} />
-            </div>
-            <div className="flex justify-center -mt-20">
-              <Image src={vector} />
-            </div>
+      <div className="grid lg:grid-cols-2">
+        <div className="img-side lg:col-span-1 hidden lg:flex mx-20 my-16 bg-rose-100 justify-center items-center rounded-2xl">
+          <div className="flex justify-center px-20  ">
+            <Image src={pestVector} />
           </div>
         </div>
-        <div className="form-side md:col-span-1 flex flex-col my-16 justify-center items-center">
+        <div className="form-side lg:col-span-1 flex flex-col my-16 justify-center items-center">
           <div className="title uppercase font-bold text-3xl text-gray-900">
             <h2>pest control</h2>
           </div>
